947d42951b87d6dd28360ffadae4dc0029351f3f
lpolon <lpolon@gmail.com>
2020-02-08 16:21:02 -0300

    bulma.css lib and react-router-dom basic setup

---
+++ src/Components/App/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Switch } from 'react-router-dom';
+import 'bulma/css/bulma.css'
+import './App.css';
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return null;
+  }
+}
