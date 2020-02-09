552c1c79672d16ef0d9cb66987662622846d11ca
lpolon <lpolon@gmail.com>
2020-02-09 13:26:49 -0300

    refactor of dogApi util closured fetchOne logic for DRYness and separeted endpoint exports for better code readability

--- src/utils/dogApi.js
+++ src/utils/dogApi.js
@@ -1,29 +1,48 @@
-export const dogApi = {
-  endpoint: 'http://localhost:5000',
+const fetchOneClosure = (endpoint) => {
+  return async (id) => {
+    try {
+      const response = await fetch(`${endpoint}/${id}`);
+      const body = await response.json();
+      return body;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+};
+
+export const breeds = {
+  endpoint: 'http://localhost:5000/breeds',
 
   async fetchAll() {
     try {
-      const response = await fetch(`${this.endpoint}/breeds`);
+      const response = await fetch(`${this.endpoint}`);
       const body = await response.json();
+      console.log(body);
       return body;
     } catch (error) {
       console.log(error);
     }
   },
-
   async fetchOne(id) {
-    try {
-      const response = await fetch(`${this.endpoint}/breed/${id}`);
-      const body = await response.json();
-      const {
-        weight: { metric: wMetric },
-        height: { metric: hMetric },
-      } = body;
-      body.weight = wMetric;
-      body.height = hMetric;
-      return body;
-    } catch (error) {
-      console.log(error);
-    }
+    const body = await fetchOneClosure(this.endpoint)(id);
+    console.log(body);
+    return body;
   },
 };
+
+export const oneBreed = {
+  endpoint: 'http://localhost:5000/breed',
+  async fetchOneDetails(id) {
+    const body = await fetchOneClosure(this.endpoint)(id);
+    const {
+      weight: { metric: wMetric },
+      height: { metric: hMetric },
+    } = body;
+    body.weight = wMetric;
+    body.height = hMetric;
+    console.log(body);
+    return body;
+  },
+};
+
+oneBreed.fetchOneDetails(6);
